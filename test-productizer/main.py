c728edfebfe46d34722b1910516acbbb432c4d0b
Lassi Piironen <lsipii@kapsi.fi>
2022-08-21 22:59:23 +0300

    docs: add openapi descs

--- test-productizer/main.py
+++ test-productizer/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
-from .services.StatFinPopulation import get_population
+from .services.StatFinPopulation import StatFinPopulationDataProduct, get_population
 
 app = FastAPI()
 
 
-@app.get("/")
-@app.get("/population")
+@app.get(
+    "/",
+    summary="redirect to /population",
+    description="A redirect to /population",
+)
+@app.get(
+    "/population",
+    summary="test/undefied/population Data Product",
+    description="A test Data Product for the population query",
+    response_model=StatFinPopulationDataProduct,
+)
 async def population():
     return await get_population()
