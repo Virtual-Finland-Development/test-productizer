b88fefa361e70af0de10580bde61e4ccccf58b94
Lassi Piironen <lsipii@kapsi.fi>
2022-08-23 11:24:50 +0300

    fix: use a diff endpoint for more years in data

--- testbed-test-productizer/services/StatFinPopulation.py
+++ testbed-test-productizer/services/StatFinPopulation.py
@@ -1,11 +1,15 @@
-from typing import List, Dict, Any, Optional
+from typing import List, Dict, Any, Optional, Callable
 from pydantic import BaseModel
 from ..utils.Requester import fetch
 
-configuration = {
-    "API_ENDPOINT": "https://statfin.stat.fi/PXWeb/api/v1",
-    "API_CODE_FOR_POPULATION_SEARCH": "M411",
-}
+"""
+The data source endpoint URL
+"""
+get_api_endpoint: Callable[
+    [str], str
+] = (
+    lambda locale: f"https://statfin.stat.fi/PXWeb/api/v1/{locale}/Kuntien_avainluvut/2021/kuntien_avainluvut_2021_aikasarja.px"
+)
 
 #
 # External API response item model(s)
@@ -49,8 +53,8 @@ class StatFinFiguresResponse(BaseModel):
 
 
 class StatFinPopulationDataProductInput(BaseModel):
-    city_query: Optional[str]
-    year: Optional[int]
+    city_query: Optional[str] = ""
+    year: Optional[int] = 2021
 
 
 #
@@ -75,9 +79,9 @@ async def get_population(city_query: str = "", year: int = 2021, locale: str = "
     The getter function for the resources list
     """
 
-    resource_url_path = f"{locale}/Kuntien_avainluvut/{year}/kuntien_avainluvut_{year}_viimeisin.px"  # @note: the path is correct for all locales
-    resource_url = f"{configuration['API_ENDPOINT']}/{resource_url_path}"
+    resource_url = get_api_endpoint(locale)
     API_code_for_area = await resolve_api_code_for_area(city_query, year, locale)
+    API_code_for_population_search = "M411"
 
     # Fetch items from the external data source API
     item = await fetch(
@@ -88,13 +92,14 @@ async def get_population(city_query: str = "", year: int = 2021, locale: str = "
             "data": {
                 "query": [
                     {
-                        "code": f"Alue {year}",
+                        "code": f"Alue 2021",
                         "selection": {"filter": "item", "values": [API_code_for_area]},
                     },
                     {
                         "code": "Tiedot",
-                        "selection": {"filter": "item", "values": [configuration["API_CODE_FOR_POPULATION_SEARCH"]]},
+                        "selection": {"filter": "item", "values": [API_code_for_population_search]},
                     },
+                    {"code": "Vuosi", "selection": {"filter": "item", "values": [year]}},
                 ],
                 "response": {"format": "json-stat2"},
             },
@@ -105,7 +110,7 @@ async def get_population(city_query: str = "", year: int = 2021, locale: str = "
 
     # Transform and return response items to data product syntax
     return StatFinPopulationDataProduct(
-        label=format_data_product_label(item), source=item.source, value=item.value[0], updated=item.updated
+        label=format_data_product_label(item, year), source=item.source, value=item.value[0], updated=item.updated
     )
 
 
@@ -117,11 +122,10 @@ async def resolve_api_code_for_area(city_query: str, year: int, locale: str) -> 
     API_code_for_area = "SSS"  # Defaut: all areas
     if len(city_query) > 0:
         search_phrase = city_query.lower().strip()
+        resource_url = get_api_endpoint(locale)
 
         figures = await fetch(
-            {
-                "url": f"{configuration['API_ENDPOINT']}/{locale}/Kuntien_avainluvut/{year}/kuntien_avainluvut_{year}_viimeisin.px"
-            },
+            {"url": resource_url},
             response_type=StatFinFiguresResponse,
             formatter=StatFinFiguresResponse.parse_obj,  # validates the response
         )
@@ -136,17 +140,17 @@ async def resolve_api_code_for_area(city_query: str, year: int, locale: str) -> 
             index = city_names.index(city_name)
             API_code_for_area = figure_variables.values[index]
         else:
-            suggestions = filter(lambda city_name: city_name.lower().__contains__(search_phrase), city_names)
+            suggestions = list(filter(lambda city_name: city_name.lower().__contains__(search_phrase), city_names))
             raise ValueError(f"City {city_query} not found: try one of {suggestions}")
 
     return API_code_for_area
 
 
-def format_data_product_label(item: StatFinPopulationResponse) -> str:
+def format_data_product_label(item: StatFinPopulationResponse, year: int) -> str:
     """
     Formats the data product label
     """
     dimensions = list(item.dimension.values())
     first_dimension_category_label = list(dimensions[0].category.label.values())[0]
     second_dimension_label = list(dimensions[1].category.label.values())[0]
-    return f"{second_dimension_label}, {first_dimension_category_label}"
+    return f"{second_dimension_label}, {first_dimension_category_label}, {year}"
