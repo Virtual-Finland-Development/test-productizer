e92b7779e726bc219cbf160963cd92d7ff558aa1
Lassi Piironen <lsipii@kapsi.fi>
2022-08-25 16:02:14 +0300

    refactor: split controller routes to files

--- testbed-test-productizer/main.py
+++ testbed-test-productizer/main.py
@@ -1,10 +1,7 @@
-from fastapi import FastAPI, HTTPException
-from fastapi.responses import RedirectResponse, ORJSONResponse
-
-from .services.StatFinPopulation import StatFinPopulationDataProduct, StatFinPopulationDataProductInput, get_population
-from pydantic import ValidationError
+from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
 from fastapi.middleware.cors import CORSMiddleware
-from typing import Optional
+from .routes import base, population
 
 app = FastAPI(default_response_class=ORJSONResponse)
 
@@ -16,44 +13,5 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
-@app.get(
-    "/",
-    summary="redirect to /docs",
-    description="A redirect to /docs",
-)
-async def root():
-    return RedirectResponse("/docs")
-
-
-@app.post(
-    "/test/undefined/population",
-    summary="test/undefined/population Data Product",
-    description="A test Data Product for the population query",
-    response_model=StatFinPopulationDataProduct,
-)
-async def population(
-    city_query: Optional[str] = None,
-    year: Optional[int] = None,
-    request: Optional[StatFinPopulationDataProductInput] = None,
-):
-    try:
-
-        #
-        # Merge request query params and body
-        #
-        params = {
-            "city_query": city_query,
-            "year": year,
-        }
-        if request is not None:
-            request_fields = request.dict()
-            for entry in params.keys():
-                if entry in request_fields and bool(request_fields[entry]):
-                    params[entry] = request_fields[entry]
-
-        return await get_population(**params)
-    except ValidationError as e:
-        raise HTTPException(status_code=422, detail=str(e))
-    except ValueError as e:
-        raise HTTPException(status_code=404, detail=str(e))
+app.include_router(base.router)
+app.include_router(population.router)
---
+++ testbed-test-productizer/routes/population.py
@@ -0,0 +1,40 @@
+from fastapi import APIRouter, HTTPException
+
+router = APIRouter()
+
+from ..services.StatFinPopulation import StatFinPopulationDataProduct, StatFinPopulationDataProductInput, get_population
+from pydantic import ValidationError
+from typing import Optional
+
+
+@router.post(
+    "/test/undefined/population",
+    summary="test/undefined/population Data Product",
+    description="A test Data Product for the population query",
+    response_model=StatFinPopulationDataProduct,
+)
+async def population(
+    city_query: Optional[str] = None,
+    year: Optional[int] = None,
+    request: Optional[StatFinPopulationDataProductInput] = None,
+):
+    try:
+
+        #
+        # Merge request query params and body
+        #
+        params = {
+            "city_query": city_query,
+            "year": year,
+        }
+        if request is not None:
+            request_fields = request.dict()
+            for entry in params.keys():
+                if entry in request_fields and bool(request_fields[entry]):
+                    params[entry] = request_fields[entry]
+
+        return await get_population(**params)
+    except ValidationError as e:
+        raise HTTPException(status_code=422, detail=str(e))
+    except ValueError as e:
+        raise HTTPException(status_code=404, detail=str(e))
