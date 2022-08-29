fddc309841c6b13afdc20b8ec4f8cbe26e6fd2b7
Lassi Piironen <lsipii@kapsi.fi>
2022-08-29 09:40:14 +0300

    fix: make the pop test data agnostic

--- tests/test_population.py
+++ tests/test_population.py
@@ -7,9 +7,8 @@ client = TestClient(app)
 def test_population():
     response = client.post("/test/Figure/Population", json={"city": "Tampere", "year": 2021})
     assert response.status_code == 200
-    assert response.json() == {
-        "population": 244223,
-        "description": "Väkiluku, Tampere, 2021",
-        "sourceName": "Tilastokeskus",
-        "updatedAt": "2022-06-17T11:52:00Z",
-    }
+
+    # Check all expected keys found in the response
+    responseKeys = response.json().keys()
+    expectedAttrs = ["population", "description", "sourceName", "updatedAt"]
+    assert all(attr in responseKeys for attr in expectedAttrs)
