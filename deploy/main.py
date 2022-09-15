63619e6e34b98046f9a715e5aa6facab65056517
Lassi Piironen <lsipii@kapsi.fi>
2022-09-15 12:23:50 +0300

    fix: the iam definitions migrate to aws native

--- deploy/main.py
+++ deploy/main.py
@@ -1,73 +1,78 @@
 import pulumi
-from pulumi_aws_native import lambda_, stepfunctions, config, iam
+import json
+
+from pulumi_aws_native import lambda_, stepfunctions, iam, config
 from productizer.utils.settings import get_setting
 
 lambda_role = iam.Role(
     "lambdaRole",
-    assume_role_policy_document="""{
-        "Version": "2012-10-17",
-        "Statement": [
-            {
-                "Action": "sts:AssumeRole",
-                "Principal": {
-                    "Service": "lambda.amazonaws.com"
-                },
-                "Effect": "Allow",
-                "Sid": ""
-            }
-        ]
-    }""",
-)  # type: ignore
-
-lambda_role_policy = iam.RolePolicy(  # type: ignore
-    "lambdaRolePolicy",
-    role=lambda_role.id,
-    policy="""{
-        "Version": "2012-10-17",
-        "Statement": [{
-            "Effect": "Allow",
-            "Action": [
-                "logs:CreateLogGroup",
-                "logs:CreateLogStream",
-                "logs:PutLogEvents"
+    assume_role_policy_document=json.dumps(
+        {
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Action": "sts:AssumeRole",
+                    "Principal": {"Service": "lambda.amazonaws.com"},
+                    "Effect": "Allow",
+                    "Sid": "",
+                }
             ],
-            "Resource": "arn:aws:logs:*:*:*"
-        }]
-    }""",
+        }
+    ),
+    policies=[
+        iam.RolePolicyArgs(
+            policy_name="lambdaRolePolicy",
+            policy_document=json.dumps(
+                {
+                    "Version": "2012-10-17",
+                    "Statement": [
+                        {
+                            "Effect": "Allow",
+                            "Action": [
+                                "logs:CreateLogGroup",
+                                "logs:CreateLogStream",
+                                "logs:PutLogEvents",
+                            ],
+                            "Resource": "arn:aws:logs:*:*:*",
+                        }
+                    ],
+                }
+            ),
+        )
+    ],
 )
 
 sfn_role = iam.Role(
     "sfnRole",
-    assume_role_policy_document="""{
-        "Version": "2012-10-17",
-        "Statement": [
-            {
-                "Effect": "Allow",
-                "Principal": {
-                    "Service": "states.%s.amazonaws.com"
-                },
-                "Action": "sts:AssumeRole"
-            }
-        ]
-    }"""
-    % config.region,  # type: ignore
-)
-
-sfn_role_policy = iam.RolePolicy(  # type: ignore
-    "sfnRolePolicy",
-    role=sfn_role.id,
-    policy="""{
-        "Version": "2012-10-17",
-        "Statement": [
-            {
-                "Effect": "Allow",
-                "Action": [
-                    "lambda:InvokeFunction"
-                ],
-                "Resource": "*"
-            }
-        ]
-    }""",
+    assume_role_policy_document=json.dumps(
+        {
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Effect": "Allow",
+                    "Principal": {"Service": f"states.{config.region}.amazonaws.com"},  # type: ignore
+                    "Action": "sts:AssumeRole",
+                }
+            ],
+        }
+    ),
+    policies=[
+        iam.RolePolicyArgs(
+            policy_name="sfnRolePolicy",
+            policy_document=json.dumps(
+                {
+                    "Version": "2012-10-17",
+                    "Statement": [
+                        {
+                            "Effect": "Allow",
+                            "Action": ["lambda:InvokeFunction"],
+                            "Resource": "*",
+                        }
+                    ],
+                }
+            ),
+        )
+    ],
 )
 
 
@@ -86,28 +91,22 @@ productizerer_fn = lambda_.Function(
     code=pulumi.AssetArchive({".": pulumi.FileArchive("../productizer")}),  # type: ignore
 )
 
-
-def getStateMachineDefinition(arn: str) -> str:
-    return (
-        """{ 
-        "Comment": "Productizerer deployment state machine",
-        "StartAt": "Productizerer",
-        "States": {
-            "Productizerer": {
-                "Type": "Task",
-                "Resource": "%s",
-                "End": true
-            }
-        }
-    }"""
-        % arn
-    )
-
-
 state_defn = state_machine = stepfunctions.StateMachine(
     "stateMachine",
     role_arn=sfn_role.arn,
-    definition=productizerer_fn.arn.apply(lambda arn: getStateMachineDefinition(arn)),  # type: ignore
+    definition_string=json.dumps(
+        {
+            "Comment": "Productizerer deployment state machine",
+            "StartAt": "Productizerer",
+            "States": {
+                "Productizerer": {
+                    "Type": "Task",
+                    "Resource": productizerer_fn.arn,
+                    "End": True,
+                }
+            },
+        }
+    ),
 )
 
 pulumi.export("state_machine_arn", state_machine.id)
