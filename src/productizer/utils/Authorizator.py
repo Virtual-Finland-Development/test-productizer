0e9c802b49ccd597523b6ee3a0db30857b470d0c
Lassi Piironen <lsipii@kapsi.fi>
2022-10-03 14:17:22 +0300

    feat: auhtorize handling updated

--- src/productizer/utils/Authorizator.py
+++ src/productizer/utils/Authorizator.py
@@ -1,9 +1,7 @@
 from typing import Union
 
 from productizer.utils.Requester import fetch
-from productizer.utils.helpers import base64_encode
 from productizer.utils.settings import get_setting, if_test_mode
-from urllib.parse import quote
 from pydantic import BaseModel
 
 
@@ -11,26 +9,21 @@ class AccessDeniedResponse(BaseModel):
     message: str
 
 
-def generate_app_context() -> str:
-    """
-    Generates a string that is used to identify the application
-    """
-    return quote(base64_encode({"appName": "Test productizer"}))
-
-
-async def authorize(authorization_header: Union[str, None]) -> None:
+async def authorize(authorization_bearer_token: Union[str, None], authorization_provider: Union[str, None]) -> None:
     """
     Throws 401 if not authorized
     """
     if if_test_mode():
         return
+
     await fetch(
         {
-            "url": f"{get_setting('AUTHORIZATION_GW_ENDPOINT_URL')}/auth/openid/authorize",
+            "url": f"{get_setting('AUTHORIZATION_GW_ENDPOINT_URL')}/auth/authorize",
             "method": "POST",
-            "data": {
-                "appContext": generate_app_context(),
-                "token": authorization_header.lstrip("Bearer ") if authorization_header else "",
+            "headers": {
+                "Authorization": authorization_bearer_token,
+                "X-Authorization-Provider": authorization_provider,
+                "X-Authorization-Provider-Context": "testbed-test-productizer",
             },
         }
     )
